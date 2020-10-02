4d129659bb8d48b335f6eaec312b2d3a3a481199
Christiana Okere <yarmasani@gmail.com>
2020-10-02 05:58:37 -0700

    updated all main

--- age.js
+++ age.js
@@ -1,15 +1,40 @@
-function myFunction() {
-    document.getElementById("formContainer").style.display = "block";}
+
+const button1 = document.querySelector('.clickHere');
+const secondPage = document.querySelector('#formContainer');
+const submitButt = document.querySelector ('[type="submit"]');
+const thirdPage = document.querySelector('#finalMsg');
+
+const displayName = document.querySelector('.displayName');
+const displayYears = document.querySelector('.displayYears');
+const displayDays = document.querySelector ('.displayDays');
+
+
+
+
+button1.addEventListener('click', function (){
+    secondPage.classList.remove('hidden');
+    thirdPage.classList.add('hidden');
+})
+
+submitButt.addEventListener('click', function (e){
+    e.preventDefault();
+
+    var yourName = document.getElementById("yourName").value;
+    var yourAge = document.getElementById("yourAge").value;
+    var ageInDays = yourAge * 365;
     
-   
-    function calcAgeDays() {
-        var yourName = document.getElementById("yourName").value;
-        var yourAge = document.getElementById("yourAge").value;
-        var ageInDays = yourAge * 365;
-        
-        if (yourName == "" || yourAge == "") {
-            alert("input Missing Field");
-            } 
-            else {alert ("Congratulations " + yourName + "!!!." + " You are " +  yourAge + " years old "  +" and you have lived " + ageInDays + " days on earth!")};
-    
-    }
+    if (yourName == "" || yourAge == "") {
+        alert("input Missing Field");
+        return;
+        } 
+        else{
+            // console.log(ageInDays);
+            displayName.textContent = "Congratulations " + yourName + "!!!";
+            displayYears.textContent = "You " + "are " + yourAge + " years old "
+            displayDays.textContent = "You " + "have " + "lived " + "for " + ageInDays + " days" + " on earth.";
+
+        }
+
+    secondPage.classList.add('hidden');
+    thirdPage.classList.remove('hidden');
+})
