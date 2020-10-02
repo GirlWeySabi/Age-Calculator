b8c11a39ceaba5c6df3c1a3c82bd7be7cc7a9ccb
Christiana Okere <yarmasani@gmail.com>
2020-10-02 03:20:07 -0700

    added age_cal2 verison2

--- age.js
+++ age.js
@@ -1,15 +1,155 @@
-function myFunction() {
-    document.getElementById("formContainer").style.display = "block";}
+// function calcAgeDays() {
+//     var yourName = document.getElementById("yourName").value;
+//     var yourAge = document.getElementById("yourAge").value;
+// var ageInDays = yourAge * 365;
     
-   
-    function calcAgeDays() {
-        var yourName = document.getElementById("yourName").value;
-        var yourAge = document.getElementById("yourAge").value;
-        var ageInDays = yourAge * 365;
-        
-        if (yourName == "" || yourAge == "") {
-            alert("input Missing Field");
-            } 
-            else {alert ("Congratulations " + yourName + "!!!." + " You are " +  yourAge + " years old "  +" and you have lived " + ageInDays + " days on earth!")};
     
-    }
+    
+  
+//     if (yourName == "" || yourAge == "") {
+//       alert("input Missing Field");
+//     } else alert(yourAge + " year-old " + yourName + "is" ageInDays  );
+//   }
+
+// function calcAgeDays() {
+//     var yourName = document.getElementById("yourName").value;
+//     var yourAge = document.getElementById("yourAge").value;
+//     var ageInDays = yourAge * 365;
+    
+  
+//     if (yourName == "" || yourAge == "") {
+//       alert("input Missing Field");
+//     } else {document.getElementById("result").innerHTML = yourName + " is " + ageInDays + " days old."};
+
+// }
+
+
+
+
+// function calcAgeDays() {
+//     var yourName = document.getElementById("yourName").value;
+//     var yourAge = document.getElementById("yourAge").value;
+//     var ageInDays = yourAge * 365;
+    
+  
+//     document.getElementById("result").innerHTML = yourName + " is " + ageInDays + " days old.";
+
+// }
+
+// THIS IS WORKING!!!
+// function myFunction() {
+//     document.getElementById("formContainer").style.display = "block";}
+
+
+
+// function calcAgeDays() {
+//     var yourName = document.getElementById("yourName").value;
+//     var yourAge = document.getElementById("yourAge").value;
+//     var ageInDays = yourAge * 365;
+    
+  
+//     alert (yourName + " is " + ageInDays + " days old.");
+
+// }
+
+
+// THIS IS ALSO WORKING!!!
+
+
+// const finalPage = document.querySelector('.finalMsg');
+
+// window.onload = function() {
+//     document.getElementById('finalMsg').style.display = 'none';
+//   };
+
+
+// function myFunction() {
+//     document.getElementById("formContainer").style.display = "block"};
+
+
+
+// function calcAgeDays() {
+//     var yourName = document.getElementById("yourName").value;
+//     var yourAge = document.getElementById("yourAge").value;
+//     var ageInDays = yourAge * 365;
+    
+//     if (yourName == "" || yourAge == "") {
+//         alert("input Missing Field");
+//         } 
+//         else 
+
+// }
+
+
+
+
+// // DISPLAY RESULT AINT WORKING!
+
+// function myFunction() {
+//     document.getElementById("formContainer").style.display = "block";}
+
+
+
+// function calcAgeDays() {
+//     var yourName = document.getElementById("yourName").value;
+//     var yourAge = document.getElementById("yourAge").value;
+//     var ageInDays = yourAge * 365;
+    
+//     if (yourName == "" || yourAge == "") {
+//         alert("input Missing Field");
+//         } 
+//         else {document.getElementById("formContainer").innerHTML = yourName + " is " + ageInDays + " days old."};
+
+// }
+
+
+
+
+
+
+
+const button1 = document.querySelector('.clickHere');
+const secondPage = document.querySelector('#formContainer');
+const submitButt = document.querySelector ('[type="submit"]');
+const thirdPage = document.querySelector('#finalMsg');
+
+const displayName = document.querySelector('.displayName');
+const displayYears = document.querySelector('.displayYears');
+const displayDays = document.querySelector ('.displayDays');
+
+
+
+
+button1.addEventListener('click', function (){
+    secondPage.classList.remove('hidden');
+    thirdPage.classList.add('hidden');
+})
+
+submitButt.addEventListener('click', function (e){
+    e.preventDefault();
+
+    var yourName = document.getElementById("yourName").value;
+    var yourAge = document.getElementById("yourAge").value;
+    var ageInDays = yourAge * 365;
+    
+    if (yourName == "" || yourAge == "") {
+        alert("input Missing Field");
+        return;
+        } 
+        else{
+            console.log(ageInDays);
+            displayName.textContent = "Congratulations " + yourName + "!!!";
+            displayYears.textContent = "You " + "are " + yourAge + " years old "
+            displayDays.textContent = "You " + "have " + "lived " + "for " + ageInDays + " days" + " on earth.";
+
+        }
+
+    secondPage.classList.add('hidden');
+    thirdPage.classList.remove('hidden');
+})
+
+
+
+
+
+       
